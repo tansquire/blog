0f299cc6a84cf98546935781d7268f3534a61849
Partha Paul <parthapaul@Parthas-MacBook-Air.local>
2020-09-01 14:49:40 +0530

    test1 commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes / routing here
+| Web Routes / routing here/
+ /
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
